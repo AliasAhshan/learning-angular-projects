a2214473e6ef4f936d634b92ed93f6473b33518a
AliasAhshan <ahshan.ahmed003@gmail.com>
2025-09-23 14:11:22 -0400

    Learned and implemented template variables for retrieving input values along with setting up viewchild signal and lifecycle components

--- 01-starting-project/src/app/shared/control/control.component.ts
+++ 01-starting-project/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
+import { AfterContentInit, Component, contentChild, ContentChild, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -12,7 +12,7 @@ import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEn
     '(click)': 'onClick()',
   }
 })
-export class ControlComponent {
+export class ControlComponent implements AfterContentInit {
   // @HostBinding('class') className = 'control';
   // @HostListener('click') onClick() {
   //   console.log('Clicked!')
@@ -20,9 +20,17 @@ export class ControlComponent {
 
   label = input.required<string>()
   private el = inject(ElementRef)
+  // @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>
+
+  private control = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
+   
+  ngAfterContentInit() {
+    //...
+  }
 
   onClick() {
     console.log("Clicked!")
     console.log(this.el)
+    console.log(this.control())
   }
 }
