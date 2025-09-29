3f5e626c69dac2425979e66674034e59510b664c
AliasAhshan <ahshan.ahmed003@gmail.com>
2025-09-29 13:53:07 -0400

    Learned about observables and implemented subscriptions to observable functions with operators, and learned the different use cases for signals/observables

--- rxjs-observables-deep-dive/src/app/app.component.ts
+++ rxjs-observables-deep-dive/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  clickCount = signal(0);
+  private destroyRef = inject(DestroyRef)
 
+  constructor() {
+    effect(() => {
+      console.log(`Clicked Button ${this.clickCount()} times.`)
+    });
+  }
+
+  
+  ngOnInit() {
+    // const subscription = interval(10000).pipe(
+    //   map((val) => val * 2),
+
+    // ).subscribe({
+    //   next: (val) => console.log(val) 
+    // });
+
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    // })
+  }
+
+  onClick() {
+    this.clickCount.update(prevCount => prevCount + 1)
+  }
 }
